d983b45d8493296ecd868ea40761eb780e38ce06
Cherron Simes <cherron@developdbycherron.com>
2021-09-30 20:54:08 -0500

    Intial heroku commit

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,11 +1,11 @@
 let APIURL = "";
 
-switch(window.location.hostname) {
-  case "localhost" || "127.0.0.1":
-    APIURL = "http://localhost:3000";
-    break;
-  case "" :
-    APIURL = ""
+switch (window.location.hostname) {
+    case "localhost" || "127.0.0.1":
+        APIURL = "http://localhost:3000";
+        break;
+    case "cs-timetrackerclient.herokuapp.com":
+        APIURL = "https://cs-timetrackerclient.herokuapp.com/";
 }
 
 export default APIURL;
