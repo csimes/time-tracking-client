da4af4430319724c0028badbda5e56ceed785d5a
Cherron Simes <cherron@developdbycherron.com>
2021-10-06 22:10:52 -0500

    Removed proxy url, cors issue still persists in deployed state

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,8 +5,7 @@ switch (window.location.hostname) {
         APIURL = "http://localhost:3000";
         break;
     case "cs-timetrackerclient.herokuapp.com":
-        APIURL =
-            "https://thingproxy.freeboard.io/fetch/https://cs-timetrackerapp.herokuapp.com";
+        APIURL = "https://cs-timetrackerapp.herokuapp.com";
         break;
         default : console.log("Unable to connect")
 }
