4eff7cc12b0268789bc941489ba65943eace1958
Cherron Simes <cherron@developdbycherron.com>
2022-12-12 22:19:00 -0600

    Updated environment for render

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,13 +1,14 @@
 let APIURL = "";
 
 switch (window.location.hostname) {
-    case "localhost" || "127.0.0.1":
-        APIURL = "http://localhost:3000";
-        break;
-    case "cs-timetrackerclient.herokuapp.com":
-        APIURL = "https://cs-timetrackerapp.herokuapp.com";
-        break;
-        default : console.log("Unable to connect")
+  case "localhost" || "127.0.0.1":
+    APIURL = "http://localhost:3000";
+    break;
+  case "time-tracking-web-service.onrender.com":
+    APIURL = "https://time-tracking-web-service.onrender.com/";
+    break;
+  default:
+    console.log("Unable to connect");
 }
 
 export default APIURL;
