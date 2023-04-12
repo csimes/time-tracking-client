9ada5fee8d4bb23fff61f7d0c0ef39ff9f79e30d
csimes <cherron@developdbycherron.com>
2023-04-11 20:40:01 -0500

    change deployed API url back to Render web service

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     APIURL = "http://localhost:3000";
     break;
   case "time-tracking-client.onrender.com":
-    APIURL = "https://gmgwwnkxeajwmmeqtsuk.supabase.co/rest/v1";
+    APIURL = "https://time-tracking-web-service.onrender.com";
     break;
   default:
     console.log("Unable to connect");
