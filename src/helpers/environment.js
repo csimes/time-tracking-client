58257ff79acca9a32cfff911a75f8a392c18c928
Cherron Simes <cherron@developdbycherron.com>
2023-06-29 20:48:27 -0500

    update environment.js with supabase API url

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -1,14 +1,14 @@
-let APIURL = "";
+let APIURL = "https://gmgwwnkxeajwmmeqtsuk.supabase.co/rest/v1";
 
-switch (window.location.hostname) {
-  case "localhost" || "127.0.0.1":
-    APIURL = "http://localhost:3000";
-    break;
-  case "time-tracking-client.onrender.com":
-    APIURL = "https://time-tracking-web-service.onrender.com";
-    break;
-  default:
-    console.log("Unable to connect");
-}
+// switch (window.location.hostname) {
+//   case "localhost" || "127.0.0.1":
+//     APIURL = "http://localhost:3000";
+//     break;
+//   case "time-tracking-client.onrender.com":
+//     APIURL = "https://time-tracking-web-service.onrender.com";
+//     break;
+//   default:
+//     console.log("Unable to connect");
+// }
 
 export default APIURL;
