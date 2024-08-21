a30ade3b9d7d20f427d821e3cc6ace83e643b3e0
csimes <cherron@developdbycherron.com>
2024-08-21 16:15:35 -0500

    add typing to APIURL in helper and refactor switch statement

--- src/helpers/environment.tsx
+++ src/helpers/environment.tsx
@@ -1,13 +1,19 @@
-let APIURL;
+let APIURL: string | undefined;
+
 switch (window.location.hostname) {
-  case "localhost" || "127.0.0.1":
+  case "localhost":
+  case "127.0.0.1":
     APIURL = "http://localhost:3000";
     break;
   case "time-tracking-client.onrender.com":
     APIURL = "https://time-tracking-web-service.onrender.com";
     break;
   default:
-    console.log("Unable to connect");
+    console.error("Unable to determine API URL");
+}
+
+if (!APIURL) {
+  throw new Error("API URL is not set");
 }
 
 export default APIURL;
